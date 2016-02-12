0350c15643ede376b695bd54422d412682dcb846
Titus Wormer <tituswormer@gmail.com>
2016-02-12 14:27:24 +0100

    Replace mocha with tape

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 'use strict';
 
-/* eslint-env node, mocha */
+/* eslint-env node */
 
 /*
  * Dependencies.
  */
 
-var assert = require('assert');
+var test = require('tape');
 var Ware = require('ware');
 var AttachWare = require('./')(Ware);
 
@@ -16,127 +16,125 @@ var AttachWare = require('./')(Ware);
 function noop() {}
 
 /*
- * Methods.
- */
-
-var equal = assert.strictEqual;
-var deepEqual = assert.deepEqual;
-
-/*
  * Tests.
  */
 
-describe('AttachWare()', function () {
-    describe('use(attacher)', function () {
-        it('should accept an attacher', function () {
-            var ware = new AttachWare().use(noop);
+test('AttachWare()', function (t) {
+    t.test('should accept an attacher', function (st) {
+        var ware = new AttachWare().use(noop);
 
-            equal(ware.attachers.length, 1);
-            equal(ware.fns.length, 0);
+        st.equal(ware.attachers.length, 1);
+        st.equal(ware.fns.length, 0);
+
+        st.end();
+    });
+
+    t.test('should accept multiple attachers', function (st) {
+        var ware = new AttachWare().use([noop, noop]);
+
+        st.equal(ware.attachers.length, 2);
+        st.equal(ware.fns.length, 0);
+
+        st.end();
+    });
+
+    t.test('should accept empty attach-ware', function (st) {
+        var ware = new AttachWare().use(new AttachWare());
+
+        st.equal(ware.attachers, undefined);
+        st.equal(ware.fns.length, 0);
+
+        st.end();
+    });
+
+    t.test('should accept attach-ware', function (st) {
+        var ware = new AttachWare().use(
+            new AttachWare().use([noop, noop])
+        );
+
+        st.equal(ware.attachers.length, 2);
+        st.equal(ware.fns.length, 0);
+
+        st.end();
+    });
+
+    t.test('should accept empty normal ware', function (st) {
+        var ware = new AttachWare().use(new Ware());
+
+        st.equal(ware.attachers, undefined);
+        st.equal(ware.fns.length, 0);
+
+        st.end();
+    });
+
+    t.test('should accept normal ware', function (st) {
+        var ware = new AttachWare().use(new Ware().use([noop, noop]));
+
+        st.equal(ware.attachers, undefined);
+        st.equal(ware.fns.length, 2);
+
+        st.end();
+    });
+
+    t.test('should attach multiple attachers in correct order', function (st) {
+        var order = [];
+
+        new AttachWare().use([function () {
+            order.push(1);
+        }, function () {
+            order.push(2);
+        }]);
+
+        st.deepEqual(order, [1, 2]);
+
+        st.end();
+    });
+
+    t.test('should attach a by `attach` returned middleware', function (st) {
+        var ware = new AttachWare();
+
+        ware.use(function () {
+            return noop;
         });
 
-        it('should accept multiple attachers', function () {
-            var ware = new AttachWare().use([noop, noop]);
+        st.equal(ware.attachers.length, 1);
+        st.equal(ware.fns.length, 1);
 
-            equal(ware.attachers.length, 2);
-            equal(ware.fns.length, 0);
+        st.end();
+    });
+
+    t.test('should invoke with the current context', function (st) {
+        var ware = new AttachWare();
+
+        st.plan(1);
+
+        ware.use(function (context) {
+            st.equal(context, ware);
         });
+    });
 
-        it('should accept empty attach-ware', function () {
-            var ware = new AttachWare().use(new AttachWare());
+    t.test('should invoke with a `context` when present', function (st) {
+        var ware = new AttachWare();
 
-            equal(ware.attachers, undefined);
-            equal(ware.fns.length, 0);
+        st.plan(1);
+
+        ware.context = {};
+
+        ware.use(function (context) {
+            st.equal(context, ware.context);
         });
+    });
 
-        it('should accept attach-ware', function () {
-            var ware = new AttachWare().use(
-                new AttachWare().use([noop, noop])
-            );
+    t.test('should pass rest-arguments', function (st) {
+        var ware = new AttachWare();
 
-            equal(ware.attachers.length, 2);
-            equal(ware.fns.length, 0);
-        });
+        st.plan(4);
 
-        it('should accept empty normal ware', function () {
-            var ware = new AttachWare().use(new Ware());
-
-            equal(ware.attachers, undefined);
-            equal(ware.fns.length, 0);
-        });
-
-        it('should accept normal ware', function () {
-            var ware = new AttachWare().use(new Ware().use([noop, noop]));
-
-            equal(ware.attachers, undefined);
-            equal(ware.fns.length, 2);
-        });
-
-        it('should attach multiple attachers in correct order', function () {
-            var order = [];
-
-            new AttachWare().use([function () {
-                order.push(1);
-            }, function () {
-                order.push(2);
-            }]);
-
-            deepEqual(order, [1, 2]);
-        });
-
-        it('should attach a by `attach` returned middleware', function () {
-            var ware = new AttachWare();
-
-            ware.use(function () {
-                return noop;
-            });
-
-            equal(ware.attachers.length, 1);
-            equal(ware.fns.length, 1);
-        });
-
-        it('should invoke with the current context', function () {
-            var ware = new AttachWare();
-            var invoked;
-
-            ware.use(function (context) {
-                equal(context, ware);
-
-                invoked = true;
-            });
-
-            equal(invoked, true);
-        });
-
-        it('should invoke with a `context` when present', function () {
-            var ware = new AttachWare();
-            var invoked;
-
-            ware.context = {};
-
-            ware.use(function (context) {
-                equal(context, ware.context);
-
-                invoked = true;
-            });
-
-            equal(invoked, true);
-        });
-
-        it('should pass rest-arguments', function () {
-            var ware = new AttachWare();
-            var invoked;
-
-            ware.use(function (zero, one, two, three) {
-                equal(zero, ware);
-                equal(one, 1);
-                equal(two, 2);
-                equal(three, 3);
-
-                invoked = true;
-            }, 1, 2, 3);
-
-            equal(invoked, true);
-        });
+        ware.use(function (zero, one, two, three) {
+            st.equal(zero, ware);
+            st.equal(one, 1);
+            st.equal(two, 2);
+            st.equal(three, 3);
+        }, 1, 2, 3);
     });
 });
