a83b48a2188ef00fecdb9177c86ee61a767ac6b8
Titus Wormer <tituswormer@gmail.com>
2016-11-04 16:56:41 +0100

    Remove file-overview docs

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module attach-ware
- * @fileoverview Middleware with configuration.
- */
-
 'use strict';
 
 /* Dependencies. */
